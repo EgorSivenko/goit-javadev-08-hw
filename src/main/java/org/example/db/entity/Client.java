3dc5eedc5ff386a87e87665c52d0f9f01748f07e
EgorSivenko <egor.siv20@gmail.com>
2023-12-24 16:13:40 +0200

    ClientService implemented✅

---
+++ src/main/java/org/example/db/entity/Client.java
@@ -0,0 +1,9 @@
+package org.example.db.entity;
+
+import lombok.Data;
+
+@Data
+public class Client {
+    private long clientId;
+    private String clientName;
+}
---
+++ src/main/java/org/example/db/services/ClientService.java
@@ -0,0 +1,110 @@
+package org.example.db.services;
+
+import org.example.db.entity.Client;
+import org.example.db.utils.Database;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ClientService {
+    private final PreparedStatement createSt;
+    private final PreparedStatement getByIdSt;
+    private final PreparedStatement setNameSt;
+    private final PreparedStatement deleteByIdSt;
+    private final PreparedStatement listAllSt;
+    private final PreparedStatement getLastInsertIdSt;
+
+    public ClientService(Connection connection) throws SQLException {
+        createSt = connection.prepareStatement("INSERT INTO clients (client_name) VALUES (?)");
+        getByIdSt = connection.prepareStatement("SELECT client_name FROM clients WHERE client_id = ?");
+        setNameSt = connection.prepareStatement("UPDATE clients SET client_name = ? WHERE client_id = ?");
+        deleteByIdSt = connection.prepareStatement("DELETE FROM clients WHERE client_id = ?");
+        listAllSt = connection.prepareStatement("SELECT client_id, client_name FROM clients");
+        getLastInsertIdSt = connection.prepareStatement("SELECT last_insert_id()");
+    }
+
+    public long create(String name) throws SQLException {
+        createSt.setString(1, name);
+
+        try {
+            createSt.executeUpdate();
+        } catch (SQLException ex) {
+            throw new IllegalArgumentException("Client name is too short");
+        }
+
+        ResultSet resultSet = getLastInsertIdSt.executeQuery();
+        if (!resultSet.next()) {
+            return -1;
+        }
+        return resultSet.getLong("last_insert_id()");
+    }
+
+    public String getById(long id) throws SQLException {
+        if (id < 1)
+            throw new IllegalArgumentException("Client id cannot be <= 0");
+
+        getByIdSt.setLong(1, id);
+
+        ResultSet resultSet = getByIdSt.executeQuery();
+        if (!resultSet.next()) {
+            throw new IllegalStateException("Client with id " + id + " doesn't exist");
+        }
+        return resultSet.getString("client_name");
+    }
+
+    public boolean setName(long id, String name) throws SQLException {
+        if (id < 1)
+            throw new IllegalArgumentException("Client id cannot be <= 0");
+
+        setNameSt.setString(1, name);
+        setNameSt.setLong(2, id);
+
+        try {
+            int update = setNameSt.executeUpdate();
+            return update != 0;
+        } catch (SQLException ex) {
+            throw new IllegalArgumentException("Client name is too short");
+        }
+    }
+
+    public boolean deleteById(long id) throws SQLException {
+        if (id < 1)
+            throw new IllegalArgumentException("Client id cannot be <= 0");
+
+        deleteByIdSt.setLong(1, id);
+
+        try {
+            int update = deleteByIdSt.executeUpdate();
+            return update != 0;
+        } catch (SQLException ex) {
+            throw new IllegalArgumentException(
+                    "Client with id " + id + " cannot be deleted due to foreign key constraint");
+        }
+    }
+
+    public List<Client> listAll() throws SQLException {
+        List<Client> clients = new ArrayList<>();
+        ResultSet resultSet = listAllSt.executeQuery();
+
+        while (resultSet.next()) {
+            var client = new Client();
+            client.setClientId(resultSet.getLong("client_id"));
+            client.setClientName(resultSet.getString("client_name"));
+            clients.add(client);
+        }
+        return clients;
+    }
+
+    public static void main(String[] args) throws SQLException {
+        ClientService clientService = new ClientService(Database.getInstance().getConnection());
+        System.out.println(clientService.create("egor"));
+        System.out.println(clientService.getById(4));
+        System.out.println(clientService.setName(10, "john"));
+        System.out.println(clientService.deleteById(8));
+        System.out.println(clientService.listAll());
+    }
+}
